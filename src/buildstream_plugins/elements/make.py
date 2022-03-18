2e26118765815f3592e8e5a2818e420dd5c10fc3
Tristan van Berkom <tristan.vanberkom@codethink.co.uk>
2022-03-18 17:16:45 +0900

    Initially adding make element
    From bst-plugins-experimental

---
+++ src/buildstream_plugins/elements/make.py
@@ -0,0 +1,52 @@
+#
+#  Copyright Bloomberg Finance LP
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Authors:
+#        Ed Baunton <ebaunton1@bloomberg.net>
+
+"""
+make - Make build element
+=========================
+This is a `BuildElement
+<https://docs.buildstream.build/master/buildstream.scriptelement.html#module-buildstream.scriptelement>`_
+implementation for using GNU make based build.
+
+.. note::
+
+   The ``make`` element is available since `format version 9
+   <https://docs.buildstream.build/master/format_project.html#project-format-version>`_
+
+Here is the default configuration for the ``make`` element in full:
+
+  .. literalinclude:: ../../../src/buildstream_plugins/elements/make.yaml
+     :language: yaml
+
+See `built-in functionality documentation
+<https://docs.buildstream.build/master/buildstream.buildelement.html#core-buildelement-builtins>`_ for
+details on common configuration options for build elements.
+"""
+
+from buildstream import BuildElement
+
+
+# Element implementation for the 'make' kind.
+class MakeElement(BuildElement):
+
+    BST_MIN_VERSION = "2.0"
+
+
+# Plugin entry point
+def setup():
+    return MakeElement
