6c4a5f69128bf806182289dd961765e3d6fa168b
Tristan van Berkom <tristan.vanberkom@codethink.co.uk>
2022-03-18 17:21:03 +0900

    Initially adding pip element
    From bst-plugins-experimental

---
+++ src/buildstream_plugins/elements/pip.py
@@ -0,0 +1,46 @@
+#
+#  Copyright (C) 2017 Mathieu Bridon
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Authors:
+#        Mathieu Bridon <bochecha@daitauha.fr>
+
+"""
+pip - Pip build element
+=======================
+A `BuildElement
+<https://docs.buildstream.build/master/buildstream.buildelement.html#module-buildstream.buildelement>`_
+implementation for installing Python modules with pip
+
+The pip default configuration:
+  .. literalinclude:: ../../../src/buildstream_plugins/elements/pip.yaml
+     :language: yaml
+
+See `built-in functionality documentation
+<https://docs.buildstream.build/master/buildstream.buildelement.html#core-buildelement-builtins>`_ for
+details on common configuration options for build elements.
+"""
+
+from buildstream import BuildElement
+
+
+# Element implementation for the 'pip' kind.
+class PipElement(BuildElement):
+
+    BST_MIN_VERSION = "2.0"
+
+
+# Plugin entry point
+def setup():
+    return PipElement
