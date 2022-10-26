e7ef46bd9f8c64bbc3db56cedc862477e6ebfca8
Tristan van Berkom <tristan.vanberkom@codethink.co.uk>
2022-10-26 17:36:06 +0900

    Cleanup license headers for initial apache release.
    This patch removes the copyright statements from license
    headers, and adds license headers to files which are missing them.

--- src/buildstream_plugins/elements/meson.py
+++ src/buildstream_plugins/elements/meson.py
@@ -1,5 +1,3 @@
-#  Copyright (C) 2017 Patrick Griffis
-#  Copyright (C) 2018 Codethink Ltd.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
