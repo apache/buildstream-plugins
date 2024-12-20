2d92cc366751290bb7b505de3eeffeba3bb06ed7
Jürg Billeter <j@bitron.ch>
2024-12-20 14:02:20 +0100

    Drop support for Python 3.8
    Python 3.8 is no longer maintained upstream and some Python packages
    have dropped support for it.

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
